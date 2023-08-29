b9e33ae7e49847ec6b2115b3516efb3515bef602
hd9 <hectorjr_805@yahoo.com>
2023-08-29 10:41:59 -0700

    updated reaction schema

--- models/thoughts.js
+++ models/thoughts.js
@@ -14,7 +14,7 @@ const thoughtSchema = new Schema(
     createdAt: {
       type: Date,
       default: Date.now,
-      get: (timestamp) => dateFormat(timestamp),
+      get: timestamp => dateFormat(timestamp),
     },
 
     userName: {
